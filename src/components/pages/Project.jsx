69bcd9fe78a7f4f7d0f9c2e9881561fb77c72dfb
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-07-08 21:54:18 -0300

    Exibição de detalhes
    Criando página para exibir os detalhes do projeto

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,31 +1,72 @@
-import styles from './Project.module.css';
+import styles from "./Project.module.css";
 
-import { useParams } from 'react-router-dom';
-import { useState, useEffect } from 'react';
-import { GoProjectSymlink } from 'react-icons/go';
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
+import Loading from "../layout/Loading";
+import Container from "../layout/Container";
 
 export default function Project() {
-    const { id } = useParams()
+  const { id } = useParams();
 
-    const [project, setProject] = useState([])
+  const [project, setProject] = useState([]);
+  const [showProjectForm, setShowProjectForm] = useState(false)
 
-    useEffect(() => {
-
-        fetch(`http://localhost:5000/projects/${id}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(resp => resp.json())
+  useEffect(() => {
+    setTimeout(() => {
+      fetch(`http://localhost:5000/projects/${id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((resp) => resp.json())
         .then((data) => {
-            setProject(data)
+          setProject(data);
         })
-        .catch(err => console.log())
+        .catch((err) => console.log());
+    }, 700);
+  }, [id]);
 
-    }, [id])
+  function ToggleProjectForm() {
+    setShowProjectForm(!showProjectForm)
+  }
 
-    return (
-        <p>{project.name}</p>
-    );
+  return (
+    <>
+      {project.name ? (
+        <div className={styles.project_details}>
+          <Container customClass="column">
+            <div className={styles.details_container}>
+              <h1>Projeto: {project.name}</h1>
+              <button className={styles.btn} onClick={ToggleProjectForm}>
+                {!showProjectForm ? 'Editar projeto' : 'Fechar'}
+              </button>
+              {!showProjectForm ? (
+                <div className={styles.project_info}>
+                    <p>
+                        <span>Categoria:</span> {project.category.name}
+                    </p>
+                    <p>
+                        <span>Total de Orçamento:</span> R${project.budget}
+                    </p>
+                    <p>
+                        <span>Total Utilizado:</span> R${project.cost}
+                    </p>
+                </div>
+              ) : (
+                <div className={styles.project_info}>
+                    <p>
+                        form
+                    </p>
+                </div>
+              )}
+            </div>
+          </Container>
+        </div>
+      ) : (
+        <Loading />
+      )}
+    </>
+  );
 }
