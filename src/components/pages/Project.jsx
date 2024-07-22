f0328436a06d57aa380cc57a673536fbb108e5a3
Mateus Moraes <mateusjr_m.a@hotmail.com>
2024-07-22 18:57:35 -0300

    Área de serviços
    Criando área de serviços, nesta etapa com adição e edição de serviços

--- src/components/pages/Project.jsx
+++ src/components/pages/Project.jsx
@@ -1,3 +1,5 @@
+import { parse, v4 as uuidv4 } from 'uuid';
+
 import styles from "./Project.module.css";
 
 import { useParams } from "react-router-dom";
@@ -5,8 +7,9 @@ import { useState, useEffect } from "react";
 
 import Loading from "../layout/Loading";
 import Container from "../layout/Container";
-import Message from "../layout/Message"
-import ProjectForm from "../project/ProjectForm"
+import Message from "../layout/Message";
+import ProjectForm from "../project/ProjectForm";
+import ServiceForm from "../service/Serviceform";
 
 export default function Project() {
   const { id } = useParams();
@@ -59,6 +62,44 @@ export default function Project() {
       })
   }
 
+  function createService(project) {
+    setMessage('')
+
+    // last service
+    const lastService = project.services[project.services.length - 1]
+
+    lastService.id = uuidv4()
+
+    const lastServiceCost = lastService.cost
+
+    const newCost = parseFloat(project.cost) + parseFloat(lastServiceCost)
+
+    // maximum value validation
+    if (newCost > parseFloat(project.budget)) {
+      setMessage('Orçamento ultrapassado, verifique o valor do serviço!')
+      setType('error')
+      project.services.pop()
+      return false
+    }
+
+    // add service cost to project cost total
+    project.cost = newCost
+
+    fetch(`http://localhost:5000/projects/${project.id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(project),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        // exibir os serviços
+        console.log(data)
+      })
+      .catch((err) => console.log(err))
+  }
+
   function ToggleProjectForm() {
     setShowProjectForm(!showProjectForm)
   }
@@ -107,7 +148,11 @@ export default function Project() {
                 {!showServiceForm ? 'Adicionar Serviço' : 'Fechar'}
               </button>
               <div className={styles.project_info}>
-                {showServiceForm && <div>formulário do serviço</div>}
+                {showServiceForm && <ServiceForm 
+                  handleSubmit={createService}
+                  btnText="Adicionar Serviço"
+                  projectData={project}
+                />}
               </div>
             </div>
             <h2>Serviços</h2>
